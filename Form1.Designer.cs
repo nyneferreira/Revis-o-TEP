9a3b5bac3e31ffdfe70e3227ef3003bc5d92414a
Janine Chaves Ferreira Assunção <nyneferreira@gmail.com>
2021-03-03 14:51:30 -0300

    Adicionar arquivos de projeto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,211 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textNomeProduto = new System.Windows.Forms.TextBox();
+            this.textQuantidade = new System.Windows.Forms.TextBox();
+            this.quantidade = new System.Windows.Forms.Label();
+            this.textValorUnitario = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.helpProvider1 = new System.Windows.Forms.HelpProvider();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textTotalCompra = new System.Windows.Forms.TextBox();
+            this.textDesconto = new System.Windows.Forms.TextBox();
+            this.textTotalFinal = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(64, 58);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(92, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nome do produto:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // textNomeProduto
+            // 
+            this.textNomeProduto.Location = new System.Drawing.Point(162, 55);
+            this.textNomeProduto.Name = "textNomeProduto";
+            this.textNomeProduto.Size = new System.Drawing.Size(100, 20);
+            this.textNomeProduto.TabIndex = 1;
+            // 
+            // textQuantidade
+            // 
+            this.textQuantidade.Location = new System.Drawing.Point(162, 97);
+            this.textQuantidade.Name = "textQuantidade";
+            this.textQuantidade.Size = new System.Drawing.Size(100, 20);
+            this.textQuantidade.TabIndex = 3;
+            // 
+            // quantidade
+            // 
+            this.quantidade.AutoSize = true;
+            this.quantidade.Location = new System.Drawing.Point(64, 100);
+            this.quantidade.Name = "quantidade";
+            this.quantidade.Size = new System.Drawing.Size(68, 13);
+            this.quantidade.TabIndex = 2;
+            this.quantidade.Text = "Quantidade: ";
+            // 
+            // textValorUnitario
+            // 
+            this.textValorUnitario.Location = new System.Drawing.Point(162, 139);
+            this.textValorUnitario.Name = "textValorUnitario";
+            this.textValorUnitario.Size = new System.Drawing.Size(100, 20);
+            this.textValorUnitario.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(64, 142);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(78, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Preço unitário: ";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(67, 184);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(134, 30);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Limpar  Dados";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textTotalCompra
+            // 
+            this.textTotalCompra.Location = new System.Drawing.Point(401, 55);
+            this.textTotalCompra.Name = "textTotalCompra";
+            this.textTotalCompra.Size = new System.Drawing.Size(100, 20);
+            this.textTotalCompra.TabIndex = 10;
+            // 
+            // textDesconto
+            // 
+            this.textDesconto.Location = new System.Drawing.Point(401, 97);
+            this.textDesconto.Name = "textDesconto";
+            this.textDesconto.Size = new System.Drawing.Size(100, 20);
+            this.textDesconto.TabIndex = 11;
+            // 
+            // textTotalFinal
+            // 
+            this.textTotalFinal.Location = new System.Drawing.Point(401, 139);
+            this.textTotalFinal.Name = "textTotalFinal";
+            this.textTotalFinal.Size = new System.Drawing.Size(100, 20);
+            this.textTotalFinal.TabIndex = 12;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(305, 62);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(90, 13);
+            this.label2.TabIndex = 14;
+            this.label2.Text = "Total da compra: ";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(305, 104);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(53, 13);
+            this.label4.TabIndex = 15;
+            this.label4.Text = "Desconto";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(305, 146);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(59, 13);
+            this.label5.TabIndex = 16;
+            this.label5.Text = "Total Final:";
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Location = new System.Drawing.Point(308, 184);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(118, 30);
+            this.btnCalcular.TabIndex = 17;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(522, 251);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textTotalFinal);
+            this.Controls.Add(this.textDesconto);
+            this.Controls.Add(this.textTotalCompra);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textValorUnitario);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textQuantidade);
+            this.Controls.Add(this.quantidade);
+            this.Controls.Add(this.textNomeProduto);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Compra";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textNomeProduto;
+        private System.Windows.Forms.TextBox textQuantidade;
+        private System.Windows.Forms.Label quantidade;
+        private System.Windows.Forms.TextBox textValorUnitario;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.HelpProvider helpProvider1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textTotalCompra;
+        private System.Windows.Forms.TextBox textDesconto;
+        private System.Windows.Forms.TextBox textTotalFinal;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button btnCalcular;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            textTotalCompra.Enabled = false;
+            textTotalFinal.Enabled = false;
+            textDesconto.Enabled = false;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int quantidade;
+            double valor_unitario;
+
+            
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            string nomeProduto;
+            int quantidade;
+            double valorUnitario, totalCompra, desconto,totalFinal;
+            nomeProduto = textNomeProduto.Text;
+
+            quantidade = Convert.ToInt32(textQuantidade.Text);
+            valorUnitario = Convert.ToDouble(textValorUnitario.Text);
+
+            totalCompra = quantidade * valorUnitario;
+
+
+            if (quantidade <= 5)
+            {
+                desconto = totalCompra * 0.02;
+                textDesconto.Text = desconto.ToString();
+                totalFinal = totalCompra - desconto;
+            }
+            else
+            {
+                desconto = 0;
+                textDesconto.Text = desconto.ToString();
+                totalFinal = totalCompra - desconto;
+            }
+
+            
+
+            textTotalCompra.Text = totalCompra.ToString();
+            textTotalFinal.Text = totalFinal.ToString();
+            
+            
+        }
+    }
+}
